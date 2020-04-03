d8000db08b48c2563dcb5c5032be92ab946be998
Joey Gauthier <digitaljedi@outlook.com>
2020-04-03 06:21:35 -0500

    error handling and stats at top

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,26 +1,53 @@
-import React, { FunctionComponent, useCallback, useState } from 'react';
+import React, { FunctionComponent, useCallback, useState, useEffect } from 'react';
 import { Button } from 'primereact/button';
+import { Growl } from 'primereact/growl';
 import Axios from 'axios';
 import DatTable from './components/Table';
 import './Main.scss';
 import moment from 'moment';
 
 const Main: FunctionComponent<any> = () => {
+  const [channel, setChannel] = useState<any>(null);
   const [data, setData] = useState([]);
   const [videos, setVideos] = useState([]);
   const [plName, setPlName] = useState('Selected Playlist');
+  const [growl, setGrowl] = useState<any>(null);
   const url = process.env.REACT_APP_PI === 'true' ? '192.168.0.152' : 'localhost';
 
+  const showGrowl = useCallback(
+    (data: any) => {
+      if (growl) {
+        // @ts-ignore
+        growl.show(data);
+      } else {
+        console.error(data.detail);
+      }
+    },
+    [growl]
+  );
+
+  const getChannelData = useCallback(() => {
+    Axios.get(`http://${url}:4001/api/channel`)
+      .then((result) => {
+        console.log('channel', result.data);
+        setChannel(result.data.items[0]);
+      })
+      .catch((error) => {
+        showGrowl({ severity: 'error', summary: 'Error fetching channel data!', detail: error });
+      });
+  }, [showGrowl, setChannel, url]);
+
   const refreshData = useCallback(() => {
+    getChannelData();
     Axios.get(`http://${url}:4001/api/playlists`)
       .then((result) => {
-        console.log('data', result);
+        console.log('data', result.data.items);
         setData(result.data.items);
       })
       .catch((error) => {
-        console.log('error', error);
+        showGrowl({ severity: 'error', summary: 'Error fetching playlist data!', detail: error });
       });
-  }, [url]);
+  }, [url, showGrowl, getChannelData]);
 
   const rowClicked = useCallback(
     (row: any) => {
@@ -32,16 +59,15 @@ const Main: FunctionComponent<any> = () => {
           setVideos(result.data.items);
         })
         .catch((error) => {
-          console.log('playlistItems error', error);
+          showGrowl({ severity: 'error', summary: 'Error fetching videos data!', detail: error });
         });
     },
-    [url]
+    [url, showGrowl]
   );
 
   const generateReport = useCallback(() => {
     Axios.get(`http://${url}:4001/api/report`)
       .then((result) => {
-        console.log('result report', result);
         const element = document.createElement('a');
         element.setAttribute(
           'href',
@@ -60,26 +86,40 @@ const Main: FunctionComponent<any> = () => {
       .catch((error) => {
         console.log('report error', error);
       });
-  }, []);
+  }, [url]);
 
-  // useEffect(() => {
-  //   refreshData();
-  // });
+  useEffect(() => {
+    if (!channel) {
+      refreshData();
+    }
+  });
 
   return (
     <div className="main">
-      <Button
-        label="Refresh Channel Data"
-        icon="pi pi-refresh"
-        className="p-button-raised"
-        onClick={refreshData}
-      />
-      <Button
-        label="Generate Report"
-        icon="pi pi-table"
-        className="p-button-secondary p-button-raised"
-        onClick={generateReport}
-      />
+      <div className="main-header">
+        <div className="main-buttons">
+          <Button
+            label="Refresh Channel Data"
+            icon="pi pi-refresh"
+            className="p-button-raised"
+            onClick={refreshData}
+          />
+          <Button
+            label="Generate Report"
+            icon="pi pi-table"
+            className="p-button-secondary p-button-raised"
+            onClick={generateReport}
+          />
+        </div>
+        {channel && (
+          <div className="main-stats">
+            <div>Subscribers: {channel?.statistics?.subscriberCount}</div>
+            <div>Views: {channel?.statistics?.viewCount}</div>
+            <div>Videos: {channel?.statistics?.videoCount}</div>
+            <div>Comments: {channel?.statistics?.commentCount}</div>
+          </div>
+        )}
+      </div>
       <div className="main-wrapper">
         <div className="playlist-table">
           <h2>Playlists</h2>
@@ -90,6 +130,7 @@ const Main: FunctionComponent<any> = () => {
           {videos?.length && <DatTable data={videos} rowClicked={rowClicked} isPlaylists={false} />}
         </div>
       </div>
+      <Growl ref={(el) => setGrowl(el)} />
     </div>
   );
 };
